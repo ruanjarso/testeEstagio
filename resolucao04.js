0d0074d3fb503403587fbdd39697792c3651f201
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2024-08-26 17:57:07 -0300

    implementação de resolucao04.js

--- resolucao04.js
+++ resolucao04.js
@@ -4,17 +4,22 @@ const itensVendidos = [
     { sku: 1003, quantidade: 5 }
 ];
 
+const estoque = [
+    { sku: 1001, quantidade: 5 },
+    { sku: 1002, quantidade: 2 },
+    { sku: 1003, quantidade: 5 }
+];
 
-
-
-
-
-
-
-
-
-
-
-function AtualizarEstoque(itensVendidos,estoque) {
-    
+/*CRIAÇÃO DA FUNÇÃO*/ 
+function atualizarEstoque(itensVendidos,estoque) {
+    itensVendidos.forEach(itemVendido => { /*percorre a lista*/ 
+        const produtoEmEstoque = estoque.find(produto => produto.sku === itemVendido.sku)  /*verifica se o produto existe em estoque*/  
+        if (produtoEmEstoque) {
+            produtoEmEstoque.quantidade -= itemVendido.quantidade /*subtrai o produto vendido do estoque*/ 
+        }
+    })
+    return estoque
 }
+
+/*VISUALIÇÃO*/
+console.log(atualizarEstoque(itensVendidos,estoque))
