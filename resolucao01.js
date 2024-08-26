0a2a165458c15ddfc6705d7f15db5f0217ac385d
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2024-08-26 14:43:49 -0300

    implementação de resolucao01.js

---
+++ resolucao01.js
@@ -0,0 +1,11 @@
+/*CRIAÇÃO DA FUNÇÃO*/
+function calcularFinaciamento(valorProduto,quantidadeParcelas,taxaDeJuros) {
+    const i = taxaDeJuros/100    /*transformando a taxa de juros para porcentagem*/
+    const valorParcela = valorProduto/quantidadeParcelas    /*descobrindo o valor da parcela*/ 
+    valorTotal = valorParcela * ((((1+i)**quantidadeParcelas) -1)/i)  /*Fórmula sendo aplicada*/ 
+    return valorTotal  /*retornado o resultado*/ 
+}
+/*VISUALIZAÇÃO*/ 
+const totalApagar = calcularFinaciamento(1000,10,2)
+console.log("O valor total a ser pago é: ", totalApagar.toFixed(2)) /*toFixed(): para rertornar somento 2 casas após a vírgula*/ 
+
